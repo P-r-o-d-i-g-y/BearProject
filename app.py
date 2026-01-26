a0acd7a38d99c8e57e9ecb75bf2f1beaa8099e74
nnm <dipperlps821@gmail.com>
2026-01-26 15:41:16 +0300

    взята модель small вместо nano, таймкод видео, iou для дублей в 50% исправить скорость видео, каждый 10ый рисовать а не пропускать, сейчас сделано наоборот

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ import cv2                                                #достает кар
 import pandas as pd                                       #нужно для Excel
 from ultralytics import YOLO                              #предобученная модель 
 import tempfile                                           #тащит видео из рам на диск для opencv
-from datetime import datetime                             #для времени обнаружения
+from datetime import datetime, timedelta                  #для времени обнаружения
 
 #настройки-------------------------
-CONFIDENCE_THRESHOLD = 0.5
+CONFIDENCE_THRESHOLD = 0.4
 
 @st.cache_resource                                          #чтобы модель не загружалась заново и осталась в памяти
 # загрузка модели с предобучением
 def load_model():
-    model = YOLO('yolov8n.pt')                              
+    model = YOLO('yolov8s.pt')                              
     return model
 
 model = load_model()
@@ -22,8 +22,6 @@ st.sidebar.header("Настройки")
 source_option = st.sidebar.selectbox("Источник видео", ("Загрузка видео", "Веб-камера"))
 
 history_data = []
-
-
 cap = None                                            #переменная файл
 
 #кнопка загрузки файла
@@ -50,6 +48,8 @@ if cap  is not None:
     
     # Счетчик кадров
     frame_count = 0 
+    last_result = None  # для хранения последней рамки
+    last_logged_sec = -1  # чтобы не спамить одну секунду
     #цикл модели-----------------------------------------
     while cap.isOpened() and not stop_button:
         
@@ -58,26 +58,44 @@ if cap  is not None:
             st.write("Видео закончилось")
             break
         frame_count += 1
-
+        #тамкод
+        msec = cap.get(cv2.CAP_PROP_POS_MSEC)
+        video_time = str(timedelta(milliseconds=int(msec))).split('.')[0]
         if frame_count % 10 != 0:
-             st_frame.image(frame, channels="BGR")
-             continue
-
-        results = model.predict(frame, conf=0.4, classes=[21])    #в базе COCO 21- Медведь
-        frame_with_boxes = results[0].plot()                      #квадраты на кадре
+            # verbose=False чтобы терминал не спамил
+            results = model.predict(
+                frame, 
+                conf=CONFIDENCE_THRESHOLD, 
+                classes=[21],
+                iou=0.5,  # убирает дубли
+                verbose=False
+            )
+            last_result = results 
+            # статистика
+            bears_count = len(results[0].boxes)
+            if bears_count > 0:
+                print(f"Кадр {frame_count}: найдено {bears_count} объектов. Уверенности: {[round(float(c), 2) for c in results[0].boxes.conf]}")
+                current_sec = int(msec / 1000)
+                
+                if current_sec != last_logged_sec:
+                    history_data.append({
+                        "Таймкод": video_time,  
+                        "Медведей": bears_count
+                    })
+                    last_logged_sec = current_sec
         
-        # СБОР СТАТИСТИКИ 
-        bears_count = len(results[0].boxes) # Сколько рамок нашел
-        if bears_count > 0:
-            history_data.append({
-                "Время": datetime.now().strftime("%H:%M:%S"),
-                "Медведей": bears_count
-            })
 
-        st_frame.image(frame_with_boxes, channels="BGR")
+        # рисуем всегда
+        if last_result is not None:
+            # старая рамка на новом кадре
+            frame_with_boxes = last_result[0].plot(img=frame)
+            st_frame.image(frame_with_boxes, channels="BGR")
+        else:
+            # если сеть еще не запускалась
+            st_frame.image(frame, channels="BGR")
     cap.release()                                                 #освобождение ресурсов
 
-    # --- ВЫВОД ОТЧЕТА (добавлено) ---
+    # вывод отчета
     if len(history_data) > 0:
         st.success("Обработка завершена.")
         df = pd.DataFrame(history_data)
